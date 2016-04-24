b1860ab9b64b21baa9557b39beb31bb79a89a5ee
Mariam <mzaim@ece-batch.ecn.purdue.edu>
2016-04-24 17:33:32 -0400

    Program runs smoothly, but zombie at the end

--- xv6-ca9/usr/test_cv.c
+++ xv6-ca9/usr/test_cv.c
@@ -2,7 +2,7 @@
 #include "stat.h"
 #include "user.h"
 
-int numThreads = 5;
+int numThreads = 1;
 int counter = 0;
 int cond_var = 5;
 
@@ -11,24 +11,22 @@ void* foo2(void);
 
 void* foo1(void){
   counter++;
-  printf(1, "\n  IN START FUNCTION1, pid: %d, tid: %d, counter: %d\n", getpid(), gettid(), counter);  
-  
+ 
   printf (1, " Thread %d Waiting on cond var\n", gettid());
-  //kthread_cond_wait(cond_var);
+  kthread_cond_wait(cond_var);
   printf (1, " Thread %d woken up on cond var\n", gettid());
   
-  kthread_exit();
+  //kthread_exit();
   return NULL;
 }
 
 void* foo2(void){
   counter++;
   int i=0;
-  printf(1, "\n  IN START FUNCTION2, pid: %d, tid: %d, counter: %d\n", getpid(), gettid(), counter);  
-  
   printf (1, " Thread %d looping\n", gettid());
+  
   while (i<100) { i++; }
-  //kthread_cond_signal(cond_var);
+  kthread_cond_signal(cond_var);
   printf (1, " Thread %d signalled all sleepers\n", gettid());
 
   kthread_exit();
@@ -38,22 +36,24 @@ void* foo2(void){
 int main (int argc, char *argv[])
 {
   int i;
-  int tids[numThreads];
+  //int tids[numThreads];
   counter = 0;
 
   printf(1, "In main, initially, counter: %d\n", counter);
 
   for (i=0; i<numThreads; i++) {
     if (gettid() == 0) { //just a protection. Remove later once thread_exit implemented.
-      if (i==4) { tids[i] = kthread_create(foo2); }
-      else      { tids[i] = kthread_create(foo1); }
+      kthread_create(foo2); 
     }
   }
 
+  if (gettid() == 0)
+    foo1();
+
   //TIDs of children start at 1, since parent's tid = 0.
-  for (i=0; i<numThreads; i++) {
-      kthread_join(tids[i]);
-  }
+  //for (i=0; i<numThreads; i++) {
+  //    kthread_join(tids[i]);
+  //}
 
   if (gettid() == 0) {
     printf(1, "In main, after threads finished, counter: %d\n", counter);
